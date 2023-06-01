7cc88e0f662a60b396d7372975e16b81c7350ad5
albuquerque-lucas <lucaslpra@gmail.com>
2023-06-01 12:40:00 -0300

    fix:Refacoring connect method from DatabaseConnection. Excluding parameters and defining variables inside the method

--- assets/Infrastructure/DatabaseConnection.php
+++ assets/Infrastructure/DatabaseConnection.php
@@ -7,10 +7,15 @@ use PDOException;
 
 class DatabaseConnection
 {
-    public static function connect(string $hostName, string $dbName, string $userName, string $password)
+    public static function connect()
     {
+        $hostName = 'localhost';
+        $dbName = 'login_system';
+        $userName = 'root';
+        $password = 'mamao123mamao';
         try {
             return new PDO("mysql:host=$hostName;dbname=$dbName", $userName, $password);
+            echo "conectado";
         } catch (PDOException $PDOException) {
             echo "Falha na conexão: " . $PDOException->getMessage() . "<br/>";
             echo "Arquivo: " . $PDOException->getFile() . "<br/>";
