7befefeeeefa48c795696a3fb5ef68c2dd1bbca7
albuquerque-lucas <lucaslpra@gmail.com>
2023-06-25 14:02:58 -0300

    Creating notFoundView

--- index.php
+++ index.php
@@ -1,21 +1,18 @@
 <?php
 require 'vendor/autoload.php';
 
-use LucasAlbuquerque\LoginSystem\Infrastructure\DatabaseConnection;
+use LucasAlbuquerque\LoginSystem\View\NotFoundView;
 
-
-$pdo = DatabaseConnection::connect();
 $pathInfo = $_SERVER['PATH_INFO'];
 $routes = require __DIR__ . '/routes/routes.php';
 
 $controllerClass = $routes[$pathInfo];
 
 if(!array_key_exists($pathInfo, $routes)){
-    echo "Erro 404";
+    $notFoundView = new NotFoundView();
+    $notFoundView->handle();
     exit();
 }
-
-
 
 $controller = new $controllerClass();
 $controller->handle();
