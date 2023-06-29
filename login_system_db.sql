5b0c3a3eff9637e67dedcaebcad734a4545cd99f
albuquerque-lucas <lucaslpra@gmail.com>
2023-06-28 22:09:23 -0300

    Updating database creation schema

--- login_system_db.sql
+++ login_system_db.sql
@@ -28,21 +28,25 @@ CREATE TABLE `sessions` (
   `sessions_id` int NOT NULL AUTO_INCREMENT,
   `sessions_token` varchar(45) DEFAULT NULL,
   `sessions_serial` varchar(45) DEFAULT NULL,
-  `sessions_datetime` varchar(10) DEFAULT NULL,
+  `sessions_datetime` varchar(40) DEFAULT NULL,
   `sessions_userid` int DEFAULT NULL,
   PRIMARY KEY (`sessions_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=125 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `sessions`
+-- Table structure for table `task_status`
 --
 
-LOCK TABLES `sessions` WRITE;
-/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
-INSERT INTO `sessions` VALUES (43,'zgQGcvbBhVXC04L1ydfsPFUjZRt6DH7M','gIjhBpdklfuH4JGSMcqZb0DEN6CrF3ve','20/12/1999',24);
-/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `task_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `task_status` (
+  `task_status_id` int NOT NULL AUTO_INCREMENT,
+  `task_status_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`task_status_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `tasks`
@@ -53,26 +57,17 @@ DROP TABLE IF EXISTS `tasks`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tasks` (
   `task_id` int NOT NULL AUTO_INCREMENT,
-  `task_name` varchar(45) DEFAULT NULL,
-  `task_description` varchar(45) DEFAULT NULL,
-  `task_status` int DEFAULT NULL,
-  `task_status_name` varchar(45) NOT NULL,
+  `task_name` varchar(500) DEFAULT NULL,
+  `task_description` varchar(500) DEFAULT NULL,
   `task_creation_date` varchar(19) DEFAULT NULL,
   `task_init_date` varchar(19) DEFAULT NULL,
   `task_conclusion_date` varchar(19) DEFAULT NULL,
-  PRIMARY KEY (`task_id`,`task_status_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `task_status_id` int DEFAULT NULL,
+  PRIMARY KEY (`task_id`),
+  KEY `task_status_id` (`task_status_id`),
+  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`task_status_id`) REFERENCES `task_status` (`task_status_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tasks`
---
-
-LOCK TABLES `tasks` WRITE;
-/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
-INSERT INTO `tasks` VALUES (9,'123','123',2,'Finalizada','2023-06-04 22:22:03','2023-06-04 22:32:41','2023-06-04 22:42:01');
-/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -87,22 +82,11 @@ CREATE TABLE `users` (
   `user_firstname` varchar(45) DEFAULT NULL,
   `user_lastname` varchar(45) DEFAULT NULL,
   `user_email` varchar(45) DEFAULT NULL,
-  `user_password` varchar(45) DEFAULT NULL,
   `user_password_hash` varchar(500) DEFAULT NULL,
   `user_status` int DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (21,'lucaslpra','Lucas','Leal','lucaslpra@gmail.com','654321','$argon2id$v=19$m=65536,t=4,p=1$ZnZEYnNoNVg2SFIyQTJnbQ$W2l0zg5JsvE18bEzjIngYzrAXHSwnqfgIXwZkjsKzag',1),(22,'teste1','Teste','Uno','lucaslpra@gmail.com','123456','$argon2id$v=19$m=65536,t=4,p=1$L2Z5OG1lTUg1ZkZnaC81Rw$vTpo/UeioK8fZR+vKMGYDTINVMzrZ+iKvA1mewbIny8',1),(24,'albuquerque.lucas','Lucas','Albuquerque','lucaslpra@gmail.com','123456','$argon2id$v=19$m=65536,t=4,p=1$TUZhMXdJaFFPV3hPbS5DZg$6R3Rz3zF0BACynAY9j1GEqR5tyn+Lj1IWMcMQh9M3EQ',1);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -113,4 +97,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-04 22:46:01
+-- Dump completed on 2023-06-28 22:07:17
