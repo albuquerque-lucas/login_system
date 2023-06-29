3203ee7bf4fd4a6a8f413fa073d2bc0c182bf198
albuquerque-lucas <lucaslpra@gmail.com>
2023-06-28 22:21:49 -0300

    database changes

--- login_system_db.sql
+++ login_system_db.sql
@@ -73,7 +73,7 @@ CREATE TABLE `tasks` (
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `users`; 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
