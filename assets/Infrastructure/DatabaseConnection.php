1da93704e67e47fe01aeb3fc8fa7dd676e0a6ac2
albuquerque-lucas <lucaslpra@gmail.com>
2023-07-06 20:59:57 -0300

    Refactoring database connectionfile

--- assets/Infrastructure/DatabaseConnection.php
+++ assets/Infrastructure/DatabaseConnection.php
@@ -10,9 +10,9 @@ class DatabaseConnection
     public static function connect()
     {
         $hostName = 'localhost';
-        $dbName = 'u663236748_login_system';
-        $userName = 'u663236748_root';
-        $password = 'Mamao123Mamao';
+        $dbName = 'login_system';
+        $userName = 'root';
+        $password = 'mamao123mamao';
         try {
             return new PDO("mysql:host=$hostName;dbname=$dbName", $userName, $password);
             echo "conectado";
